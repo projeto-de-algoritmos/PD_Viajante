66af90aa5b27508945e8db7b998a9bb3b1d3f4ab
bruno henrique <bh.bsb2011@hotmail.com>
2023-07-11 20:30:35 -0300

    knapsack function return max value total weight items
    Co-authored-by: João Gabriel Elvas Ribeiro <joaog.elvas@gmail.com>

--- app/src/knapsackPd.ts
+++ app/src/knapsackPd.ts
@@ -1,14 +1,16 @@
 interface Item {
     weight: number;
     value: number;
+    name: string;
   }
   
   interface KnapsackResult {
     maxValue: number;
     totalWeight: number;
+    selectedItems: Item[]
   }
-
-export default function knapsack(items: Item[], capacity: number): KnapsackResult {
+  
+  export default function knapsack(items: Item[], capacity: number): KnapsackResult {
     const n = items.length;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const dp: number[][] = new Array(n + 1);
@@ -32,15 +34,17 @@ export default function knapsack(items: Item[], capacity: number): KnapsackResul
     let maxValue = dp[n][capacity];
     let totalWeight = 0;
     let remainingCapacity = capacity;
+    const selectedItems: Item[] = [];
   
     for (let i = n; i > 0 && maxValue > 0; i--) {
-      if (maxValue >= items[i - 1].value) {
-        const { weight } = items[i - 1];
-        totalWeight += weight;
-        maxValue -= items[i - 1].value;
-        remainingCapacity -= weight;
+      if (maxValue !== dp[i - 1][remainingCapacity]) {
+        const item = items[i - 1];
+        selectedItems.push(item);
+        totalWeight += item.weight;
+        maxValue -= item.value;
+        remainingCapacity -= item.weight;
       }
     }
   
-    return { maxValue: dp[n][capacity], totalWeight };
+    return { maxValue: dp[n][capacity], totalWeight, selectedItems };
   }
